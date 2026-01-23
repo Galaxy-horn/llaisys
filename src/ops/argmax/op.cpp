9ad025da5d58679d09f1d9c0eca8f241faef9425
Galaxy-horn <1697807685@qq.com>
2026-01-23 15:35:53 +0800

    作业2.1

--- src/ops/argmax/op.cpp
+++ src/ops/argmax/op.cpp
@@ -2,6 +2,9 @@
 
 namespace llaisys::ops {
 void argmax(tensor_t max_idx, tensor_t max_val, tensor_t vals) {
-    TO_BE_IMPLEMENTED();
+    CHECK_SAME_DEVICE(max_idx, max_val, vals);
+    if (vals->deviceType() == LLAISYS_DEVICE_CPU) {
+        return cpu::argmax(max_idx->data(), max_val->data(), vals->data(), vals->dtype(), vals->numel());
+    }
 }
 } // namespace llaisys::ops
